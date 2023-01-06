38916d68c1bdf57142c616248d733209c33a805d
Lukassza <lukasszafranek@gmail.com>
2023-01-06 11:33:31 +0100

    blog v6 Sans l'onglet catégorie !!!

--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -68,6 +68,7 @@ class Categorie
         return $this;
     }
 
+    // ! ???
     public function getCateToUser(): ?User
     {
         return $this->CateToUser;
--- src/Form/CategorieType.php
+++ src/Form/CategorieType.php
@@ -14,7 +14,6 @@ class CategorieType extends AbstractType
         $builder
             ->add('Nom')
             ->add('Date')
-            
         ;
     }
 
